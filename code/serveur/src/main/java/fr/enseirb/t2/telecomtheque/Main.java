0282384f9331d8a6ea5ba50bd5b97320a4b6cfd3
tgourdel <thibaut.gourdel@gmail.com>
2016-03-26 01:45:55 +0100

    Serveur de base Jersey

---
+++ code/serveur/src/main/java/fr/enseirb/t2/telecomtheque/Main.java
@@ -0,0 +1,51 @@
+package fr.enseirb.t2.telecomtheque;
+
+import org.glassfish.grizzly.http.server.CLStaticHttpHandler;
+import org.glassfish.grizzly.http.server.HttpHandler;
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import java.io.IOException;
+import java.net.URI;
+
+/**
+ * Main class.
+ *
+ */
+public class Main {
+    // Base URI the Grizzly HTTP server will listen on
+    public static final String BASE_URI = "http://0.0.0.0:8081/api/";
+
+    /**
+     * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
+     * @return Grizzly HTTP server.
+     */
+    public static HttpServer startServer() {
+        // create a resource config that scans for JAX-RS resources and providers
+        // in com.telecom package
+        final ResourceConfig rc = new ResourceConfig().packages("fr.enseirb.t2.telecomtheque");
+
+        // create and start a new instance of grizzly http server
+        // exposing the Jersey application at BASE_URI
+        HttpServer httpServer = GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), rc);
+        HttpHandler httpHandler = new CLStaticHttpHandler(HttpServer.class.getClassLoader(), "/");
+        httpServer.getServerConfiguration().addHttpHandler(httpHandler, "/");
+        return httpServer;
+    }
+
+    /**
+     * Main method.
+     * @param args
+     * @throws IOException
+     */
+    @SuppressWarnings("deprecation")
+	public static void main(String[] args) throws IOException {
+        final HttpServer server = startServer();
+        System.out.println(String.format("Jersey app started with WADL available at "
+                + "%sapplication.wadl\nHit enter to stop it...", BASE_URI));
+        System.in.read();
+        server.stop();
+    }
+}
+
